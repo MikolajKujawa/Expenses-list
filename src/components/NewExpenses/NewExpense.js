864faf748a9b5a62305ef07e15af5b725b72e7c2
Mikołaj Kujawa <mkkujawix@gmail.com>
2021-06-03 22:36:58 +0200

    Create showing form button Create simple messages for users

--- src/components/NewExpenses/NewExpense.js
+++ src/components/NewExpenses/NewExpense.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+
+  const [formHandler, setFormHandler] = useState(false)
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString()
     };
     props.onAddExpense(expenseData);
+    setFormHandler(false)
   };
+  
+  const showForm = () => {
+    setFormHandler(true)
+  }
+
+  const stopShowForm = () => {
+    setFormHandler(false)
+  }
 
   return (
     <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!formHandler && <button onClick={showForm}>Add New Expense</button>}
+      {formHandler && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} stopShowingForm={stopShowForm}/>}
     </div>
   );
 };
